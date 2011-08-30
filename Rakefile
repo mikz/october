147f805303ef6308e4354ff1689afa3f5d9ceb2a
Michal Cichra <m@o2h.cz>
2011-08-31 01:39:07 +0200

    ssl

--- Rakefile
+++ Rakefile
@@ -4,8 +4,13 @@ require 'boot'
 require 'october'
 
 task :start do
-  bot = October::Base.new
-  bot.start
+  @bot = October::Base.new
+  @bot.start
+end
+
+task :console  => [:start] do
+  # FIXME: will not get there.. need to use plugin with hook on :connect 
+  @bot.pry
 end
 
 task :default => :start
--- lib/october/config.rb
+++ lib/october/config.rb
@@ -2,16 +2,22 @@ module October
   module Config
 
     def configuration(name = ENV['OCTOBER_ENV'].presence || 'default')
-      if @configuration ||= YAML.load_file(File.join('config', 'irc.yml'))
-        @configuration
-      end
-
-      @configuration[name]
+      @configuration ||= YAML.load_file(File.join('config', 'irc.yml')).with_indifferent_access
+      @configuration[name.to_sym]
     end
 
     def load_config!
       configuration.each_pair do |key, value|
-        config.send key.dup << '=', value
+        # FIXME: make DRY
+        # FIXME: only one level - bad, only to support SSL nested values for now
+        if value.is_a? Hash
+          subconfig = config.send key.dup
+          value.each_pair do |key, value|
+            subconfig.send key.dup << '=', value
+          end
+        else
+          config.send key.dup << '=', value
+        end
       end
     end
 
