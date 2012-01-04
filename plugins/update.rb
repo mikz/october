fe82696c81a4efa756ac6b16bccdbba92ab1fa5d
Michal Cichra <michal@o2h.cz>
2012-01-04 14:16:47 +0100

    added commands to remotely execute commands

--- plugins/update.rb
+++ plugins/update.rb
@@ -3,6 +3,11 @@ class Update
 
   match /selfupdate$/, method: :selfupdate
   match /seppuku$/, method: :seppuku
+  match /spawn (.+)$/, method: :spawn
+  match /kill (-9 )?(.+)$/, method: :kill
+  match /running$/, method: :running
+
+  $SPAWNS = []
 
   def selfupdate(m)
     m.reply "starting selfupdate..."
@@ -16,4 +21,30 @@ class Update
     sleep(1)
     exit(0)
   end
+
+  def running(m)
+    m.reply "running processes: #{$SPAWNS.join(", ")}"
+  end
+
+  def kill(m, kill, pid)
+    pid = pid.to_i
+
+    signal = kill ? 'KILL' : 'TERM'
+    Process.kill(signal, pid)
+    m.reply "sent #{signal} to process #{pid}"
+  end
+
+  def spawn(m, command)
+    pid = Process.spawn(command)
+
+    $SPAWNS.push pid
+    m.reply "spawned command '#{command}' with pid #{pid}"
+
+    Thread.new do
+      Process.wait(pid)
+      $SPAWNS.delete(pid)
+      m.reply "process #{pid} exited with status #{$?.exitstatus}"
+    end
+  end
+
 end
