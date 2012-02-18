91ad46f2582869b72439e7ce597d40862ce41bbe
Michal Cichra <michal@o2h.cz>
2012-02-18 15:49:12 +0100

    fix boot

--- boot.rb
+++ boot.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
 
-Bundler.require :default, ENV['RACK_ENV'], ENV['OCTOBER_ENV']
+Bundler.require *[:default, ENV['RACK_ENV'], ENV['OCTOBER_ENV']].compact
 
 $:.unshift File.expand_path('lib')
 $:.unshift File.expand_path('plugins')
