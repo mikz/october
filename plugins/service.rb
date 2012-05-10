2a07c4fae8bf3f23cabaa65ce033190cdc1b2d82
Michal Cichra <michal@o2h.cz>
2012-05-10 11:33:00 +0200

    really don't highlight itself

--- plugins/service.rb
+++ plugins/service.rb
@@ -5,7 +5,7 @@ class Service
    match 'guys', method: :guys
 
    def guys m
-     users = m.channel.users.keys - %w{october}
+     users = m.channel.users.keys.reject{|u| u == bot }
      m.reply users.join(', ')
    end
 end
