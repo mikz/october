c25548dc53daf712c7b695c57e45a3901412fddd
Michal Cichra <michal@o2h.cz>
2012-11-19 18:36:33 +0100

    rspec cleanup & preparation

--- spec/plugins/hudson/reporter_spec.rb
+++ spec/plugins/hudson/reporter_spec.rb
@@ -4,9 +4,8 @@ require 'hudson/reporter'
 describe Hudson::Reporter do
 
   describe "#report" do
-    let(:report) { Hudson::Reporter.new(test).report }
     let(:test) { mock(:test, project: 'name', number: 42, failures: ['first','second']) }
-    subject { report }
+    subject(:report) { Hudson::Reporter.new(test).report }
 
     its(:header) { should include 'name/42' }
     its(:length) { should == 2 }
@@ -17,11 +16,19 @@ describe Hudson::Reporter do
     let(:first) { mock(:first, project: 'first', number: 1, failures: ['first','second']) }
     let(:fifth) { mock(:fifth, project: 'fifth', number: 5, failures: ['second','third']) }
 
-    let(:diff) { Hudson::Reporter.new([first, fifth]).diff }
-    subject { diff }
+    subject(:diff) { Hudson::Reporter.new([first, fifth]).diff }
 
     its(:header) { should include "first/1 <=> fifth/5" }
     its(:length) { should == 3 }
     its(:to_s) { should include diff.header, '- first', 'second', '+ third' }
+  end
+
+  describe "#respond" do
+    let(:reporter) { Hudson::Reporter.new(test) }
+    let(:stub_test) { stub_request(:get, 'http://localhost:8080/job/test/42/consoleText') }
+
+    before do
+      stub_test
+    end
   end
 end
