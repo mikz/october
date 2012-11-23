fa8c4c106f1ec1521c4f356f58b3a1fe99a1d0f5
Michal Cichra <michal@o2h.cz>
2012-11-23 17:06:14 +0100

    fix fortune plugin

--- plugins/fortune.rb
+++ plugins/fortune.rb
@@ -1,13 +1,19 @@
-gem 'typhoeus'
+require 'httpclient'
+require 'httpclient/include_client'
 
 class Fortune
+  extend HTTPClient::IncludeClient
+  include_http_client
+
   include October::Plugin
 
   match /fortune?$/, method: :fortune
 
+  FORTUNE_API = 'http://www.fortunefortoday.com/getfortuneonly.php'
+
   register_help 'fortune', 'fortune YO!'
   def fortune(m)
-    response = Typhoeus::Request.get "http://www.fortunefortoday.com/getfortuneonly.php"
+    response = self.class.http_client.get(FORTUNE_API)
     m.reply response.body.strip
   end
 end
