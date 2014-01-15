118fe680b6b73409a9eb7408afbac42425a9f6e4
Raimon Grau <raimon@3scale.net>
2014-01-15 17:02:15 +0100

    guys with punctuation

--- plugins/service.rb
+++ plugins/service.rb
@@ -2,7 +2,7 @@ class Service
   include October::Plugin
   register_help 'guys [msg]', 'focuses all users in channel'
 
-   match /guys(\s+.+?)?$/, method: :guys
+   match /guys(\W+.+?)?$/, method: :guys
 
    def guys(m, msg)
      users = m.channel.users.keys.reject{|u| u == bot }
