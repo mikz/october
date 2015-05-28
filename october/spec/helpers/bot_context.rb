fd5be61bb13f287f803f604e2c64bb8765d829a9
Michal Cichra <michal@o2h.cz>
2015-05-28 08:20:33 +0200

    silent logging in tests

--- october/spec/helpers/bot_context.rb
+++ october/spec/helpers/bot_context.rb
@@ -13,9 +13,13 @@ RSpec.shared_context :bot do
 
   let(:bot) { October::Bot.new }
 
+  let(:log) { StringIO.new }
+  let(:logger) { Cinch::Logger.new(log) }
+
   before do
     irc.queue = queue
 
+    bot.loggers.replace([logger])
     bot.set_nick 'bot'
     bot.sync(:user, 'bot', true)
     bot.sync(:host, 'localhost', true)
